98ed58d8da64dab2f609c6d0e418432eba8a2a39
weartstudio <balazs@weart.hu>
2022-10-24 21:36:48 +0200

    theme

--- front-page.php
+++ front-page.php
@@ -4,4 +4,6 @@ $context = Timber::context();
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 
+$context['booking'] = do_shortcode('[wpbs id="1" title="yes/no" legend="yes/no" language="auto" form_id="1"]');
+
 Timber::render( 'frontPage.twig', $context );
